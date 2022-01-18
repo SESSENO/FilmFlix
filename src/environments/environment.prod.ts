f15a508b269b0ff74694e9d8fa8fbc52a9820464
Sesseno <sfsena@gmail.com>
2022-01-18 14:58:08 -0300

    Commit Inicial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'filmflix-cf5f0',
+    appId: '1:463645050747:web:90d1d042ed9510987b5fde',
+    storageBucket: 'filmflix-cf5f0.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDJVSYrVSjgaILiCycoB9zya1DhOxRg43o',
+    authDomain: 'filmflix-cf5f0.firebaseapp.com',
+    messagingSenderId: '463645050747',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'filmflix-cf5f0',
+    appId: '1:463645050747:web:90d1d042ed9510987b5fde',
+    storageBucket: 'filmflix-cf5f0.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDJVSYrVSjgaILiCycoB9zya1DhOxRg43o',
+    authDomain: 'filmflix-cf5f0.firebaseapp.com',
+    messagingSenderId: '463645050747',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
