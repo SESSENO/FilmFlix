cae53dc78656d861f8e36722fd3c83f36c003dbe
Sesseno <sfsena@gmail.com>
2022-01-20 15:39:39 -0300

    Subida de segurança

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  user$?: Observable<User | undefined>;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { //
+    this.user$ = this.authService.user;
+  }
+
+  logout(){
+    this.authService.logout();
   }
 
 }
