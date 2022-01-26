9fcdb4ab84a4f18e473e3f204ffa65ead01a3267
Sesseno <sfsena@gmail.com>
2022-01-25 23:51:12 -0300

    VersãoOk_24Jan2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
   {
     path: 'auth',
     loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule),
   },
   {
-    path: '',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    path: '**',
+    component: NotFoundComponent
   },
   // {
   //   path: 'perfil',
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,16 +1,26 @@
+import { NotFoundComponent } from './../core/components/not-found/not-found.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+const redirectLoggedInToHOme = () => redirectLoggedInTo(['/']);
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    ...canActivate(redirectLoggedInToHOme),
   },
   {
     path: 'registro',
-    component: SignupComponent
+    component: SignupComponent,
+    ...canActivate(redirectLoggedInToHOme),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   }
 ];
 
